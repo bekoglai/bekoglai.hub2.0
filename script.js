b15e261be5daa7d16aa0363b6830871d837df407
bekoglai <prusskosovetskij@gmail.com>
2025-04-01 16:13:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ function startGame() {
         alert('Ты играешь! Но на самом деле это просто эффект 😂');
     }, 1000);
 }
+const pet = document.getElementById('pet');
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    
+    pet.style.left = mouseX + 'px';
+    pet.style.top = mouseY + 'px';
+
+    if (mouseX > window.innerWidth / 2) {
+        pet.style.animation = 'petMood 0.5s infinite';
+    } else {
+        pet.style.animation = '';
+    }
+});
+function teleport() {
+    window.location.href = "https://pornhub.com";  // Замените ссылку на ту, куда хотите перенести пользователя
+}
+function playSound() {
+    let audio = new Audio('https://uwupad.me/sound/9297#');  // Замените на свой звуковой файл
+    audio.play();
+}
+const magneticButton = document.querySelector('.magnetic-button button');
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    magneticButton.style.left = mouseX + 'px';
+    magneticButton.style.top = mouseY + 'px';
+});
